4eab0bbaf71ccb824d244b8db580d74de440ba90
hg051510 <hg051510@naver.com>
2022-05-11 00:10:18 +0900

    [2022-05-10(화)] 비즈니스 요구사항 데이터 : 회원 ID, 이름 기능 : 회원 등록, 조회 데이터 저장소 선정 X 회원 객체, 회원 리포지토리 인터페이스, 회원 리포지토리 메모리 구현체 작성

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));  // Optional로 null 가능성
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {     // 자바에서 실무에선 list 많이 씀
+        return new ArrayList<>(store.values());
+    }
+}
