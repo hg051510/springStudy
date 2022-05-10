e2ea2fed50ea236580967d595a4bef54d22da2a7
hg051510 <hg051510@naver.com>
2022-05-11 00:44:29 +0900

    [2022-05-11(수)] 회원 리포지토리 메모리 구현체 테스트 작성

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,66 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach  // Test 끝나고 호출, 콜백 메소드
+    public void afterEach() {
+        repository.clearStore();    // test 끝날때마다 데이터 초기화
+    }                               // test는 서로 순서, 의존없이 실행돠어야 함
+
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();  // Optional 꺼낼 때 사용, 별로 좋은 방법은 아님
+        //System.out.println("result = " + (result == member));
+        //Assertions.assertEquals(member, null);
+        assertThat(member).isEqualTo(result);
+
+    }
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring1");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+
+    }
+}
